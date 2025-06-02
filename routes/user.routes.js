0a02df5687fee831de4d124bf71f58448af2d38f
Jannat226 <jannat@mca.christuniversity.in>
2025-06-02 15:14:50 -0700

    getting user profile, resetPassword,ForgotPassword

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,10 +3,17 @@ import {
   loginUser,
   registerUser,
   verifyUser,
+  getMe,
+  logout,
 } from "../controller/user.controller.js";
+import { isLoggedIn } from "../middleware/auth.middleware.js";
 const router = express.Router();
 router.post("/register", registerUser);
+
 router.get("/verify/:token", verifyUser);
 router.post("/login", loginUser);
+
+router.get("/me", isLoggedIn, getMe);
+router.get("/logout", isLoggedIn, logout);
 
 export default router;
